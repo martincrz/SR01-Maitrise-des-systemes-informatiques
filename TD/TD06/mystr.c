3af8a1cd29d064c1b01fee2760e8152d4f2375cf
martincrz <115504744+martincrz@users.noreply.github.com>
2023-04-26 15:10:28 +0200

    Create mystr.c

---
+++ TD/TD06/mystr.c
@@ -0,0 +1,21 @@
+/* sapin*/
+
+#include "mystrchrn.h"
+
+int mystrchrn(char *s, char c)
+
+{
+ char *p;
+  int i, n;
+p=s;
+
+n=0;
+while(*p != '\0') 
+   {
+	if (*p==c) n++;
+ 	p++;
+   }
+
+return(n);
+
+}
