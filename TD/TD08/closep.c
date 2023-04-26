ae71f684dd17281744b5f041a26ee69ed4f98624
martincrz <115504744+martincrz@users.noreply.github.com>
2023-04-26 15:12:32 +0200

    Create closep.c

---
+++ TD/TD08/closep.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+main() {
+int pid,i,status;
+int fd,fd2;
+
+fd=open("toto.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
+printf( "avant close\n");
+close(1);
+printf("apres close\n");
+dup (fd);
+printf("apres dup\n");
+close(fd);
+printf("Le 4eme printf...\n");
+}
